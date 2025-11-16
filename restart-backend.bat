bbb03a9ce5c0fc347aa3a996ba0c82c623646e50
Atharava <siatharva@gmail.com>
2025-11-16 21:55:41 +0530

    Remove unwanted files

--- restart-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Stopping backend server...
-taskkill /F /IM node.exe 2>nul
-timeout /t 3 /nobreak >nul
-echo Starting backend server...
-cd backend
-start npm run dev
-echo Backend restarted!
